df5f68ae20f580e1ef35a1515e1f3ff7100ad14d
SimonStefenelGrouillard <106660362+SimonStefenelGrouillard@users.noreply.github.com>
2022-06-02 10:00:29 +0200

    Create codeETHZurich1.py

---
+++ codeETHZurich1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 09:11:54 2022
+
+@author: simonstefenel
+"""
+
+import numpy as np 
+
+def fonctionTest():
+    for i in range (10):
+        print("hello Word")
